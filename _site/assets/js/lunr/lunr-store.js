b95a01a33d1ca607c0b756ee4bbbd55fe013e8ff
Hwangyerin <lucialu02@naver.com>
2025-07-21 10:06:00 +0900

    게시물 사이드바 고정 및 포스트 오류 수정

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[샘플] 목차 고정 테스트",
+        "excerpt":"TOC 고정 샘플 이 글은 목차 고정(toc_sticky)을 테스트하기 위한 샘플입니다. 아래는 테스트용 소제목입니다. 1. Jekyll이란? Jekyll은 정적 웹사이트 생성기입니다. 2. Minimal Mistakes 테마 이 테마는 매우 커스터마이징이 쉽고, 블로그나 포트폴리오에 적합합니다. 3. TOC 작동 조건 목차가 보이기 위해서는 toc: true가 필요하고, 고정되게 하려면 toc_sticky: true가 있어야 합니다. 4. CSS가 필요할...","categories": ["Github Blog"],
+        "tags": ["toc","jekyll","minimal-mistakes"],
+        "url": "/github%20blog/fixed-toc/",
+        "teaser": null
+      },]
